aba4802bc21cf951155379fc55a40aba052a3d8e
mustofacahya <71838569+mustofacahya@users.noreply.github.com>
2021-07-31 00:48:25 +0700

    Create lazyadsense.js

---
+++ lazyadsense.js
@@ -0,0 +1,14 @@
+<!--noptimize-->
+<script type="text/javascript">
+function downloadJSAtOnload() {
+var element = document.createElement("script");
+element.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8656293326732758";
+document.body.appendChild(element);
+}
+if (window.addEventListener)
+window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+</script>
+<!--/noptimize-->
